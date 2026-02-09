47b89664a0d0a521defaaadcc29ebff770d0ac23
Mohamed Almatry <moalmatry2000@gmail.com>
2026-02-09 19:36:47 +0200

    feature: end the game

---
+++ Scenes/Fox/Fox.gd
@@ -0,0 +1,26 @@
+class_name Fox
+extends Area2D
+
+signal point_scored
+
+@export var speed: float = 600.00
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sounds: AudioStreamPlayer2D = $Sounds
+
+
+func _physics_process(delta: float) -> void:
+	var move: float = Input.get_axis("ui_left", "ui_right")
+	# if Input.is_action_pressed("ui_left"):
+	# 	move -= speed
+	# if Input.is_action_pressed("ui_right"):
+	# 	move += speed
+	if !is_zero_approx(move):
+		sprite_2d.flip_h = move > 0
+	position.x += move * delta * speed
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Dice:
+		sounds.play()
+		area.queue_free()
+		point_scored.emit()
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,13 +1,61 @@
 extends Node2D
 
+const DICE = preload("uid://65mx062n3tkj")
+const GAME_OVER = preload("uid://djdvnpiuk0pc1")
+const MARGIN: float = 80.0
+const STOPPABLE_GROUP := "stoppable"
+var _point:int = 0
+@onready var spawn_timer: Timer = $Pausable/SpawnTimer
+@onready var score_label: Label = $ScoreLabel
+@onready var music: AudioStreamPlayer = $Music
+@onready var pausable: Node = $Pausable
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	get_tree().paused = false
+	update_score_label()
+	spawn_dice()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func spawn_dice() -> void:
+	var new_dice: Dice = DICE.instantiate()
+	var vpr: Rect2 = get_viewport_rect()
+	var new_x: float = randf_range(vpr.position.x + MARGIN, vpr.end.x - MARGIN)
+	new_dice.position = Vector2(new_x, -MARGIN)
+	new_dice.game_over.connect(_on_dice_game_over)
+	pausable.add_child(new_dice)
+
+
+func update_score_label() -> void:
+	score_label.text = "%04d" % _point
+
+
+func pause_all() -> void:
+	spawn_timer.stop()
+	var to_stop: Array[Node] = get_tree().get_nodes_in_group(STOPPABLE_GROUP)
+	for item in to_stop:
+		item.set_physics_process(false)
 
 
 func _on_dice_game_over() -> void:
-	print("Over")  # Replace with function body.
+	pause_all()
+	music.stop()
+	music.stream = GAME_OVER
+	music.play()
+	get_tree().paused = true
+	
+
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_dice()
+
+
+func _on_fox_point_scored() -> void:
+	_point += 1
+	update_score_label()
